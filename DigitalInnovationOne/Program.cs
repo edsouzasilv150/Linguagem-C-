87f8210846879cfca4b6c115783a30d761093905
Edilson Sousa <edsouzasilv@hotmail.com>
2021-06-12 22:30:57 -0300

    Excluindo submodulos do Git

---
+++ DigitalInnovationOne/Program.cs
@@ -0,0 +1,24 @@
+﻿// CRIANDO UMA APLICAÇÃO CONSOLE.
+
+using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello World
+            // Console.WriteLine("Hello World!");
+
+            //-------------------------------------------------
+
+            // Laço de repetição.
+            int numeroDeVezes = 5;
+
+            for (int i=0; i < numeroDeVezes; i++) {
+                Console.WriteLine($"Bem-vindo ao curso de .Net {i}");
+            }
+        }
+    }
+}
---
+++ EstruturasDeDados/Program.cs
@@ -0,0 +1,22 @@
+﻿// ESTRUTURA DE DADOS - PILHA
+using System;
+using EstruturasDeDados.pilha_pratos;
+
+namespace EstruturasDeDados {
+    class Program {
+        static void Main(string[] args) {
+            var pratos = new Pilha();
+            pratos.Empilha("Prato 01");
+            pratos.Empilha("Prato 02");
+            pratos.Empilha("Prato 03");
+            pratos.Empilha("Prato 04");
+            pratos.Empilha("Prato 05");
+            Console.WriteLine(pratos.Desempilha());
+            Console.WriteLine(pratos.Desempilha());
+            Console.WriteLine(pratos.Desempilha());
+            Console.WriteLine(pratos.Desempilha());
+            Console.WriteLine(pratos.Desempilha());
+            Console.WriteLine(pratos.Desempilha());
+        }
+    }
+}
